3f53f4e9f4ffb3efc0fe19ac2c23cd34669ed990
cyx <1219951647@qq.com>
2023-11-16 13:18:36 +0800

    feat: 链表学习-判断一个链表是否为回文结构

--- 06_数据结构与算法/左程云算法课/04链表/判断一个链表是否为回文结构.js
+++ 06_数据结构与算法/左程云算法课/04链表/判断一个链表是否为回文结构.js
@@ -3,3 +3,33 @@
  * 1、遍历链表入栈，然后出栈，和原链表比较
  * 2、因为栈是先进后出，所以可以实现逆序，链表逆序的值和原链表相同，说明是回文
  */
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const header1 = new Node(2);
+header1.next = new Node(3);
+header1.next.next = new Node(3);
+
+console.log(isPalindrome1(header1));
+
+function isPalindrome1(head) {
+    const stack = [];
+    let cur = head;
+    while (cur) {
+        stack.push(cur.value);
+        cur = cur.next;
+    }
+    cur = head;
+    while (cur) {
+        if (cur.value !== stack.pop()) {
+            return false;
+        }
+        cur = cur.next;
+    }
+    return true;
+}
