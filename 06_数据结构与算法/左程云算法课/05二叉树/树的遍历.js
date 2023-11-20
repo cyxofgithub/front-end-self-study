33f44eccd8d5473631f0c8b86d538d1c6645dfd4
cyx <1219951647@qq.com>
2023-11-20 13:07:18 +0800

    feat: 二叉树学习-先序非递归遍历

--- 06_数据结构与算法/左程云算法课/05二叉树/树的遍历.js
+++ 06_数据结构与算法/左程云算法课/05二叉树/树的遍历.js
@@ -1,3 +1,61 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const node1 = new Node(1);
+node1.left = new Node(2);
+node1.right = new Node(3);
+node1.left.left = new Node(4);
+node1.left.right = new Node(5);
+node1.right.left = new Node(6);
+node1.right.right = new Node(7);
+
 // 递归遍历
+const preOrder = (root) => {
+    if (!root) return;
+    console.log(root.val);
+    preOrder(root.left);
+    preOrder(root.right);
+};
+preOrder(node1); // 1\2\4\5\3\6\7
+
+const inOrder = (root) => {
+    if (!root) return;
+    inOrder(root.left);
+    console.log(root.val);
+    inOrder(root.right);
+};
+// inOrder(node1); // 4\2\5\1\6\3\7
+
+const postOrder = (root) => {
+    if (!root) return;
+    postOrder(root.left);
+    postOrder(root.right);
+    console.log(root.val);
+};
+// postOrder(node1); // 4\5\2\6\7\3\1
 
 // 非递归遍历
+const preOrder1 = (root) => {
+    if (!root) return;
+    const stack = [root];
+
+    while (stack.length) {
+        const curNode = stack.pop();
+        console.log(curNode.val);
+
+        // 先右后左的方式压栈出栈就优先拿到左节点
+        if (curNode.right) {
+            stack.push(curNode.right);
+        }
+
+        if (curNode.left) {
+            stack.push(curNode.left);
+        }
+    }
+};
+// preOrder1(node1); // 1\2\4\5\3\6\7
