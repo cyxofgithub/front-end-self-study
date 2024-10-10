12487ee1064d3759bba5623de186bf558c89e15f
cyx <1219951647@qq.com>
2024-10-10 11:36:35 +0800

    doc: 加深理解

--- 06_数据结构与算法/左程云算法课/01排序算法/05.二分查找.js
+++ 06_数据结构与算法/左程云算法课/01排序算法/05.二分查找.js
@@ -1,28 +1,27 @@
 // 题目：有序的数组里面找到比目标数大的值(条件是>)，如果有重复给最左侧那个数字(条件成了>=)
 // 复杂度：log2N， 8个数最多被二分三次， 4， 2， 1
 const arr = [1, 1, 1, 2, 3, 4, 5, 7, 9];
-const target = 2;
+const target = 1;
 
-function getAns(arr, target) {
+function binarySearch(arr, target) {
     let left = 0;
     let right = arr.length - 1;
+    let result = -1;
+
+    if (!arr.length) return result;
 
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
 
-        if (arr[mid] <= target) {
-            left = mid + 1;
-        } else {
+        if (arr[mid] > target) {
+            result = mid;
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
 
-    if (left >= arr.length) {
-        // 超出了范围说明没搜到
-        return -1;
-    } else {
-        return arr[left];
-    }
+    return result;
 }
 
-console.log(getAns(arr, target));
+console.log(binarySearch(arr, target));
