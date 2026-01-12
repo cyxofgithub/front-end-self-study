6f44c54aa8e1e9f0ee2b15922ef9840f53c2c65a
chenyuanxin <chenyuanxin@cvte.com>
2026-01-12 23:14:40 +0800

    doc: 更新链表反转算法实现，添加详细注释与参考链接，增强代码可读性与学习资源

--- interview/2025/code/三、链表操作/1、反转链表（LeetCode 206）.js
+++ interview/2025/code/三、链表操作/1、反转链表（LeetCode 206）.js
@@ -11,7 +11,8 @@ class ListNode {
         let pre = null;
         let cur = this;
 
-        while(cur) {
+        // 口诀「存后继、指前驱、双前移」。
+        while (cur) {
             const next = cur.next;
             cur.next = pre;
             pre = cur;
@@ -24,28 +25,26 @@ class ListNode {
     // 递归法
     reverse2() {
         const traverse = (head) => {
-            // 终止条件
+            // 1. 终止条件：空或尾，直接回
             if (!head || !head.next) return head;
 
             // 1. 先递归处理后续节点，得到反转后的新头节点
             const newHead = traverse(head.next);
-            
-            // 2. 再调整当前节点与后继节点的指向
-            const next = head.next;
-            next.next = head; // 让后继节点指向自己
-            head.next = null; // 断开原指向（避免循环）
 
+            // 2. 再调整当前节点与后继节点的指向
+            head.next.next = head; // 让后继节点指向自己
+            head.next = null; // 断开原指向（避免循环）
 
             // 3. 返回反转后的新头节点（始终是原链表的最后一个节点）
             return newHead;
-        }
+        };
 
-        return traverse({...this});
+        return traverse({ ...this });
     }
 }
 
 const node1 = new ListNode(1);
-const node2= new ListNode(2);
+const node2 = new ListNode(2);
 const node3 = new ListNode(3);
 
 node1.next = node2;
@@ -53,7 +52,5 @@ node2.next = node3;
 
 // const newNode = node1.reverse()
 // console.log(newNode);
-const newNode2 = node1.reverse2()
+const newNode2 = node1.reverse2();
 console.log(newNode2);
-
-
