288d76f0e929f6097e358826056a1d819e3bda6c
chenyuanxin <chenyuanxin@cvte.com>
2026-08-03 16:04:15 +0800

    doc: top100-35

---
+++ 面经汇总/2026gap/top100/二分查找/35. 搜索插入位置.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        const mid = Math.floor((r + l) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+
+    return l;
+};
